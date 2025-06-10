25f30d88d47f7532cd0d5e30f85af13d3374cbbd
oommensy <steffy.a.oommen@intel.com>
2025-06-10 11:18:12 -0700

    Adding PipelineLoader support

--- tools/visual-pipeline-and-platform-evaluation-tool/pipeline.py
+++ tools/visual-pipeline-and-platform-evaluation-tool/pipeline.py
@@ -1,6 +1,9 @@
 import math
 from pathlib import Path
 from typing import List
+import os
+import yaml
+import importlib
 
 
 class GstPipeline:
@@ -35,3 +38,33 @@ class GstPipeline:
             raise ValueError("Bounding Boxes is not defined")
 
         return self._bounding_boxes
+    
+class PipelineLoader:
+    @staticmethod
+    def list():
+        """Return available pipeline folder names (not display names)."""
+        pipelines_dir = Path("pipelines")
+        return [
+            name for name in pipelines_dir.iterdir()
+            if name.is_dir() and not name.name.startswith("_")
+        ]
+    @staticmethod
+    def config(pipeline_name: str) -> dict:
+        """Return full config dict for a pipeline."""
+        config_path = Path("pipelines") / pipeline_name / "config.yaml"
+        if not config_path.exists():
+            raise FileNotFoundError(f"{config_path} not found")
+        return yaml.safe_load(config_path.read_text()) #TODO - validate it has description etc.
+    @staticmethod
+    def load(pipeline_name: str, metadata_only: bool = False):
+        """Load pipeline class and config, or just metadata.name"""
+        config = PipelineLoader.config(pipeline_name)
+        if metadata_only:
+            return config.get("metadata", {}).get("name", pipeline_name)
+        #class_name = "".join(word.capitalize() for word in pipeline_name.split("_")) + "Pipeline"
+        class_name = config.get("metadata", {}).get("class")
+        if not class_name:
+            raise ValueError(f"Pipeline {pipeline_name} does not have a class defined in config.yaml")
+        module = importlib.import_module(f"pipelines.{pipeline_name}.pipeline")
+        pipeline_cls = getattr(module, class_name)
+        return pipeline_cls(), config
